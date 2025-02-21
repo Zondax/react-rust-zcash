3b8530c456ddcfa499de9c40141ffa4691ef1966
neithanmo <neithanmo@gmail.com>
2025-02-21 13:10:37 -0600

    Add logging in native lib and improve bindings

--- native_rust_lib/src/lib.rs
+++ native_rust_lib/src/lib.rs
@@ -14,24 +14,59 @@ pub extern "C" fn calculate_fee(
 /// cbindgen:ignore
 #[cfg(target_os = "android")]
 pub mod android {
+    use android_logger::{Config, FilterBuilder};
+    use jni::sys::{jint, jlong};
+    use jni::JNIEnv;
+    use log::info;
+    use log::LevelFilter;
+
     use crate::calculate_fee;
     use jni::objects::JClass;
-    use jni::sys::jint;
-    use jni::JNIEnv;
 
     #[no_mangle]
     pub unsafe extern "C" fn Java_expo_modules_myrustmodule_MyRustModule_calculateFee(
-        _env: JNIEnv,
+        mut env: JNIEnv,
         _class: JClass,
         a: jint,
         b: jint,
         c: jint,
         d: jint,
-    ) -> jint {
+    ) -> jlong {
+        android_logger::init_once(
+            Config::default()
+                .with_max_level(LevelFilter::Info)
+                .with_tag("RustModule"),
+        );
+
+        info!("calculateFee called with inputs: {} {} {} {}", a, b, c, d);
+
+        if a < 0 || b < 0 || c < 0 || d < 0 {
+            info!("Negative input values detected");
+            let exception_class = env
+                .find_class("java/lang/IllegalArgumentException")
+                .unwrap();
+            env.throw_new(exception_class, "Negative values are not allowed")
+                .unwrap();
+            return 0;
+        }
+
         let n_tin = a as usize;
         let n_tout = b as usize;
         let n_spend = c as usize;
         let n_sout = d as usize;
-        calculate_fee(n_tin, n_tout, n_spend, n_sout) as jint
+
+        let result = calculate_fee(n_tin, n_tout, n_spend, n_sout);
+        info!("Fee calculation result: {}", result);
+
+        match result {
+            fee if fee <= i64::MAX as u64 => fee as jlong,
+            _ => {
+                info!("Fee calculation overflow");
+                let exception_class = env.find_class("java/lang/ArithmeticException").unwrap();
+                env.throw_new(exception_class, "Fee calculation resulted in overflow")
+                    .unwrap();
+                0
+            }
+        }
     }
 }
