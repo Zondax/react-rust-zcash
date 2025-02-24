abd7bca6cb597183cfe0258652e23c417c52aac9
neithanmo <neithanmo@gmail.com>
2025-02-24 15:04:54 -0600

    Add transparent output definition compatible with C

---
+++ native_rust_lib/src/transparent_output.rs
@@ -0,0 +1,30 @@
+use std::ffi::{c_char, CStr};
+
+use zcash_primitives::{legacy::Script, transaction::components::Amount};
+
+use crate::{parser::parse_script, ZcashError};
+
+#[repr(C)]
+pub struct TransparentOutputInfo {
+    address: *const c_char,
+    value: u64,
+}
+
+impl TransparentOutputInfo {
+    pub fn address(&self) -> Result<Script, ZcashError> {
+        unsafe {
+            let address_str = CStr::from_ptr(self.address)
+                .to_str()
+                .map_err(|_| ZcashError::InvalidArgument)?;
+            parse_script(address_str)
+        }
+    }
+
+    pub fn amount(&self) -> Result<Amount, ZcashError> {
+        Amount::from_u64(self.value).map_err(|_| ZcashError::InvalidArgument)
+    }
+
+    pub fn any_null(&self) -> bool {
+        self.address.is_null()
+    }
+}
