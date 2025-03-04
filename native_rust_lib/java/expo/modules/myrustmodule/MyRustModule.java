1db50bca5d8caa4d16f9f1abc9cdf13f3b7b93a0
neithanmo <neithanmo@gmail.com>
2025-03-03 19:34:19 -0600

    Update java classes for signature and transaction finalize

--- native_rust_lib/java/expo/modules/myrustmodule/MyRustModule.java
+++ native_rust_lib/java/expo/modules/myrustmodule/MyRustModule.java
@@ -17,6 +17,8 @@ public class MyRustModule {
     public static native int addTransparentInput(long builderId, TransparentInput input);
     public static native int addTransparentOutput(long builderId, TransparentOutput output);
     public static native byte[] buildTransaction(long builderId, String spendPath, String outputPath, int txVersion);
+    public static native int addSignatures(long builderId, Signatures signatures);
+    public static native byte[] finalizeTransaction(long builderId);
     public static native String getErrorDescription(int errorCode);
     public static native byte[] getInitTxData(InitData initData);
 }
